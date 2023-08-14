fc3b8bdbd2bf22abb9cf1583afb47bd9c191879e
Lummiense <lummiense@gmail.com>
2023-08-14 17:12:54 +0300

    Init client

---
+++ OrderApp.Client/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using OrderApp.Client.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp.Client.ViewModels
+{
+    /// <summary>
+    /// ViewModel для главного окна.
+    /// </summary>
+    internal class MainWindowViewModel:ViewModel
+    {
+
+        private string _title ="Заказы";
+        /// <summary>
+        /// Заголовок окна.
+        /// </summary>
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);            
+        }
+    }
+}
+
